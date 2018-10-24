4997e7ba57be6f430a6ae780daefbff92b46b121
willian <williantian1995@gmail.com>
2018-10-24 16:06:38 +0800

    新增detail页面样式

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
-  <header id="header">
-      <template v-if="!isLogin">
+  <header v-bind:class="{login: isLogin,'no-login': !isLogin}">
+      <template  v-if="!isLogin" >
         <h1>let's share</h1>
         <p>精品博客汇集</p>
         <div class="buttons">
@@ -9,9 +9,9 @@
         </div>
       </template>
       <template v-if="isLogin">
-        <p>let's share</p>
-        <i class="el-icon-edit"></i>
-        <img src="">
+        <h1>let's share</h1>
+        <i class="edit el-icon-edit"></i>
+        <img class="avatar" src="https://secure.gravatar.com/avatar/ff4317f0f9be382a2bca6ac3aded778a?s=50&r=pg&d=https%3A%2F%2Fdeveloper.mozilla.org%2Fstatic%2Fimg%2Favatar.png">
       </template>
   </header>
 </template>
@@ -20,7 +20,7 @@
 export default {
   data () {
     return {
-      isLogin: false,
+      isLogin: true,
     }
   }
 }
@@ -29,7 +29,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less">
 @import "../assets/base.less";
-#header{
+header.no-login{
   padding: 0 12% 30px 12%;
   display: grid;
   justify-items: center;
@@ -40,6 +40,37 @@ export default {
     font-size: 40px;
     margin: 60px 0 0 0;
     text-transform: uppercase;/*里面的文本都是大写*/
+  }
+  p{
+    margin: 15px 0 0 0;
+    color: #fff;
+  }
+  .buttons{
+    margin-top: 20px;
+  }
+}
+header.login{
+  display: flex;
+  align-items: center;
+  background: @bgColor;
+
+  h1{
+    margin: 0;
+    padding: 0;
+    color: #fff;
+    font-size: 40px;
+    text-transform: uppercase;
+    flex: 1;
+  }
+  .edit{
+    color: #fff;
+    font-size: 30px;
+  }
+  .avatar{
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-left: 15px;
   }
 }
 </style>
--- src/pages/Detail/template.vue
+++ src/pages/Detail/template.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="detail">
-      博客详情
+    <section class="user-info">
+      <img class="avatar" src="https://secure.gravatar.com/avatar/ff4317f0f9be382a2bca6ac3aded778a?s=50&r=pg&d=https%3A%2F%2Fdeveloper.mozilla.org%2Fstatic%2Fimg%2Favatar.png">
+      <h3>1111</h3>
+      <p><router-link to="/user">个人信息</router-link> 发布于三天前</p>
+    </section>
+    <section class="article"></section>
   </div>
 </template>
 
 <script src="./template.js"></script>
 
-<style src="./template.css"></style>
+<style lang="less" src="./template.less"></style>
