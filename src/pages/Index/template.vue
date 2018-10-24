667df4acb946f20e6fd739f4073ed8a88ab0e648
willian <williantian1995@gmail.com>
2018-10-24 14:30:43 +0800

    增加index页面的grid布局

--- src/pages/Index/template.vue
+++ src/pages/Index/template.vue
@@ -1,9 +1,63 @@
 <template>
   <div id="index">
-      首页
+      <section class="blog-pasts">
+        <div class="item">
+          <figure class="avatar">
+            <img src="https://secure.gravatar.com/avatar/ff4317f0f9be382a2bca6ac3aded778a?s=50&r=pg&d=https%3A%2F%2Fdeveloper.mozilla.org%2Fstatic%2Fimg%2Favatar.png">
+            <figcaption>jack</figcaption>
+          </figure>
+          <h3>哈哈哈哈哈哈 <span>刚刚</span></h3>
+          <p>哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</p>
+        </div>
+        <div class="item">
+          <figure class="avatar">
+            <img src="https://secure.gravatar.com/avatar/ff4317f0f9be382a2bca6ac3aded778a?s=50&r=pg&d=https%3A%2F%2Fdeveloper.mozilla.org%2Fstatic%2Fimg%2Favatar.png">
+            <figcaption>jack</figcaption>
+          </figure>
+          <h3>哈哈哈哈哈哈 <span>刚刚</span></h3>
+          <p>哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</p>
+        </div>
+      </section>
   </div>
 </template>
 
 <script src="./template.js"></script>
 
 <style src="./template.css"></style>
+<style lang="less">
+@import "../../assets/base.less";
+#index{
+  .item{
+    display: grid;
+    grid: auto auto / 80px 1fr;
+    
+    .avatar{
+      grid-column: 1;
+      grid-row: 1 / span2;
+      justify-self: center;
+      text-align: center;
+
+      img{
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+      }
+      figcaption{
+        font-size: 12px;
+        color: @textLightColor;
+      }
+    }
+    h3{
+      grid-column: 2;
+      grid-row: 1;
+
+      &>span{
+        color: @textLightColor;
+        font-size: 12px;
+        font-weight: normal;
+      }
+    }
+  }
+}
+</style>
+
