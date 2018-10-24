987761b88a4a419fc8880153a0d4196e92b0718d
willian <williantian1995@gmail.com>
2018-10-24 17:09:36 +0800

    my界面grid布局

--- src/pages/Detail/template.vue
+++ src/pages/Detail/template.vue
@@ -2,7 +2,7 @@
   <div id="detail">
     <section class="user-info">
       <img class="avatar" src="https://secure.gravatar.com/avatar/ff4317f0f9be382a2bca6ac3aded778a?s=50&r=pg&d=https%3A%2F%2Fdeveloper.mozilla.org%2Fstatic%2Fimg%2Favatar.png">
-      <h3>1111</h3>
+      <h3>jack</h3>
       <p><router-link to="/user">个人信息</router-link> 发布于三天前</p>
     </section>
     <section class="article"></section>
--- src/pages/My/template.vue
+++ src/pages/My/template.vue
@@ -1,9 +1,44 @@
 <template>
   <div id="my">
-      我的页面
+    <section class="user-info">
+      <img class="avatar" src="https://secure.gravatar.com/avatar/ff4317f0f9be382a2bca6ac3aded778a?s=50&r=pg&d=https%3A%2F%2Fdeveloper.mozilla.org%2Fstatic%2Fimg%2Favatar.png">
+      <h3>jack</h3>
+    </section>
+    <section>
+      <div class="item">
+        <div class="date">
+          <span class="day">20</span>
+          <span class="month">5月</span>
+          <span class="year">2018</span>
+        </div>
+        <h3>这是标题</h3>
+        <p>这是内容哈哈哈哈哈哈哈哈哈哈哈哈
+          这是内容哈哈哈哈哈哈哈哈哈哈哈哈
+          这是内容哈哈哈哈哈哈哈哈哈哈哈哈
+          这是内容哈哈哈哈哈哈哈哈哈哈哈哈
+        </p>
+        <div class="action">
+          <router-link to="/edit">编辑</router-link>
+          <a href="#">删除</a>
+        </div>
+      </div>
+      <div class="item">
+        <div class="date">
+          <span class="day">20</span>
+          <span class="month">5月</span>
+          <span class="year">2018</span>
+        </div>
+        <h3>这是标题</h3>
+        <p>这是内容哈哈哈哈哈哈哈哈哈哈哈哈</p>
+        <div class="action">
+          <router-link to="/edit">编辑</router-link>
+          <a href="#">删除</a>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script src="./template.js"></script>
 
-<style src="./template.css"></style>
+<style lang="less" src="./template.less"></style>
