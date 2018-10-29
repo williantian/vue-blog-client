9f15e9b0d3d08f06e56dea78a3b505ddf9ef6aa9
willian <williantian1995@gmail.com>
2018-10-29 14:00:55 +0800

    修改打包路径

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/vue-blog-client/dist',
     proxyTable: {},
 
     // Various Dev Server settings
